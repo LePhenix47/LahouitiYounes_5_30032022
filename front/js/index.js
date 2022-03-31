89e5d7941f47298a26f5e3ef7df899882673fd5e
Lahouiti Younes <loller763@gmail.com>
2022-03-31 14:56:02 +0200

    Optimisation du code index.js avec les fonctions asynchrones

--- front/js/index.js
+++ front/js/index.js
@@ -1,4 +1,4 @@
-console.log("Bonjour monde");
+/*console.log("Bonjour monde");
 fetch("http://localhost:3000/api/products")
   .then(function (res) {
     return res.json();
@@ -34,5 +34,40 @@ fetch("http://localhost:3000/api/products")
   .catch(function (error) {
     console.error(error);
   });
+*/
 
+  
+  console.log("Hello world");
+
+  let itemsSection = document.getElementById("items");
+
+  //La fonction products permet d'afficher dynamiquement les produits à notre page d'accueil
+  async function products(){
+    try{
+      const urlProductsAPI = "http://localhost:3000/api/products"; //On ajoute l'URL dans une constante
+      let response = await fetch(urlProductsAPI); //On attend la réponse du fetch pour savoir si le statut du code est 200
+      let sofaProductsList = await response.json(); //On crée la variable sofaProductList qui va contenir le tableau d'objet de l'API au format JSON → Liste de nos produits
+
+      for(sofaProduct of sofaProductsList){ //On fait une boucle pour parcourir le tableau
+        itemsSection.innerHTML += ` 
+        <a href="./product.html?id=${sofaProduct._id}">
+        <article>
+          <img src="${sofaProduct.imageUrl}" alt="${sofaProduct.altTxt}">
+          <h3 class="productName">${sofaProduct.name}</h3>
+          <p class="productDescription">${sofaProduct.description}</p>
+        </article>
+      </a>
+        `;
+       
+      }
+
+    }catch(error){
+      console.error("ERREUR TROUVEE= " + error);
+    }
+  }
+
+  products();
+  
+  
+  
 
