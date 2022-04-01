9070530856e6aec106b42eaebb7cd0382326081c
Lahouiti Younes <loller763@gmail.com>
2022-04-01 11:25:21 +0200

    Ajout du script pour les produits

--- front/js/product.js
+++ front/js/product.js
@@ -1,16 +1,3 @@
-console.log("Buongiorno mondo");
-
-//IDEE: Ce que l'on peut faire c'est récupérer l'id du produit et ensuite lui afficher ses details
-
-let itemImage = document.getElementsByClassName("item__img")[0]; //vu qu'on récupère un TABLEAU d'éléments et que je ne veux que la 1ère valeur on ajoute l'index du tableau → [0]
-
-let itemTitle = document.getElementById("title");
-let itemPrice = document.getElementById("price");
-let itemDescription = document.getElementById("description");
-
-let itemColors = document.getElementById("colors"); 
-
-const urlProductsAPI = "http://localhost:3000/api/products";
 /*
 function showProductDetails() {
  
@@ -38,27 +25,64 @@ function showProductDetails() {
 //-------------------------------
 */
 
+console.log("Buongiorno mondo");
 
-async function showProductDetails(){
-    try{
+//IDEE: Ce que l'on peut faire c'est récupérer l'id du produit et ensuite lui afficher ses details
+
+let itemImage = document.getElementsByClassName("item__img")[0]; //vu qu'on récupère un TABLEAU d'éléments et que je ne veux que la 1ère valeur on ajoute l'index du tableau → [0]
+
+let itemTitle = document.getElementById("title");
+let itemPrice = document.getElementById("price");
+let itemDescription = document.getElementById("description");
+
+let itemColors = document.getElementById("colors");
+
+const urlProductsAPI = "http://localhost:3000/api/products";
+
+//Cette fonction permet de nous retourner les paramètres d'une URL
+function getParameter(parameterName) {
+  let parameters = new URLSearchParams(window.location.search);
+  switch (
+    parameters.has(parameterName) //Switch case au cas où le paramètre est trouvé
+  ) {
+    case true:
+      return parameters.get(parameterName); //Retourne la VALEUR du paramètre de l'URL
+      break;
+
+    case false:
+      console.log(parameterName + " n'a pas été trouvé dans l'URL de la page!");
+      break;
+    default:
+      false;
+  }
+}
+
+async function showProductDetails() {
+  try {
     let response = await fetch(urlProductsAPI);
     let sofaProductsList = await response.json();
-     console.log(sofaProductsList);
-     
+    console.log(sofaProductsList);
+
+    console.log(getParameter("id"));
+
+    for (sofaProduct of sofaProductsList) {
+      if (getParameter("id") === sofaProduct._id) {
         itemImage.innerHTML = `
         <img src="${sofaProduct.imageUrl}" alt="${sofaProduct.altTxt}">
                 `;
         itemTitle.textContent = sofaProduct.name;
         itemPrice.textContent = sofaProduct.price;
         itemDescription.textContent = sofaProduct.description;
-      
-
-    }catch(error){
-        console.error(error);
+        for (color of sofaProduct.colors) {
+          itemColors.innerHTML += `
+          <option value="${color}">${color}</option>
+          `;
+        }
+      }
     }
-  
+  } catch (error) {
+    console.error(error);
+  }
 }
-
-
 
 showProductDetails();
