3aa108a255a9c8ea69efbaacebed004930c1ddb8
Lahouiti Younes <loller763@gmail.com>
2022-04-04 11:23:35 +0200

    Ajout d'une classe pour les enregistrer dans le panier et début de construction de fonction qui permet de vérifier Si (ID et COULEUR == même & mais QUANTITé est différente → Change uniquement la quantité du produit) + Si (ID + COULEUR + QUANTITé == même et qu'lle eiste déjà → Supprime doublon)

--- front/js/product.js
+++ front/js/product.js
@@ -13,6 +13,8 @@ let itemDescription = document.getElementById("description");
 
 let itemColors = document.getElementById("colors");
 
+let addToCartButton = document.getElementById("addToCart");
+
 const urlProductsAPI = "http://localhost:3000/api/products/";
 
 //Cette fonction permet de nous retourner la valeur du paramètre dans l'URL
@@ -32,8 +34,6 @@ let productId = getParameter("id");
 
 async function showProductDetails() {
   try {
-    
-
     let response = await fetch(urlProductsAPI + productId);
     let couchProduct = await response.json();
 
@@ -68,24 +68,116 @@ async function showProductDetails() {
   }
 }
 
-
-
 showProductDetails();
 
 
 
 
-//---------------TEST----------------//
+
+
+
+
+
+//Ajoute les produits 1 par 1 !!!!Faut faire plutot un tableau d'objets w/: ID, QUANTITé Prouit & COULEUR produit
+function addedToCart(productObject) {
+  let listOfProducts = getProducts();
+  listOfProducts.push(productObject);
+  registerProducts(listOfProducts);
+}
+
+/*
+Récupère les produits dans un tableau
+*/
+function getProducts() {
+  let listOfProducts = localStorage.getItem("listOfProducts");
+  if (listOfProducts == null) {
+    return [];
+  } else {
+    return JSON.parse(listOfProducts);
+    //'Parse'r parce que l'on a un GETTER → On reçoit un objet JSON stringifié et on veut récupérer variable + type de la variable
+  }
+}
+
+//Enregistre les produits localment dans un tableau d'objet
+function registerProducts(listOfProducts) {
+  localStorage.setItem("listOfProducts", JSON.stringify(listOfProducts));
+  //'Stringify'er parce que l'on a un SETTER → le localStorage DOIT enregistrer les types complexes (tableaux & objets) au format texte sinon on perd le type
+}
+
+function verifyProductRedundancy(listOfProducts){
+  let listOfProducts = localStorage.getItem("listOfProducts");
+  JSON.parse(listOfProducts);
+
+  for(product of listOfProducts){
+    let {id, color, quantity} = product;  
+  }
+}
+
+
+
+
+
+
+
+//Cette classe permet d'ajouter des instances d'objet d'un produit
+class classProductCartDetails {
+  constructor(id, color, quantity) {
+    this.id = id;
+    this.color = color;
+    this.quantity = quantity;
+  }
+}
 
 let quantityProduct = document.getElementById("quantity");
 let valid = true;
 
-itemColors.addEventListener("change", function(event){
-  let colorValue = event.target.value;
+let colorValue = 0;
+let quantityValue = 0;
+
+
+//On écoute les évènements lorsqu'on change de couleur et de quantité 
+itemColors.addEventListener("change", function (event) {
+  colorValue = event.target.value;
   console.log(colorValue);
 });
 
-quantityProduct.addEventListener("input", function(event){
-  let quantityValue = event.target.value;
+
+
+quantityProduct.addEventListener("input", function (event) {
+  quantityValue = event.target.value;
   console.log(quantityValue);
+});
+
+
+
+//La faudra envoyer la commande dans le panier en le stockant avec w/ WebStorage → localStorage + preventDefault du bouton commander dans l'Event listener
+//Il doit enregistrer → L'id du canapé ainsi que: Le prix, le nom, le nombre d'articles,
+
+//Ecoute l'évènement du click sur le bouton "Ajouter au panier" et le sauvegrade localement l'objet
+
+/*
+
+!ATTENTION: Faut changer la fonction parcequ'elle ajoute le même produit
+
+*/ 
+addToCartButton.addEventListener("click", function () {
+  if (quantityValue > 0 && quantityValue <= 100 && colorValue != 0) {
+    let objectProduct = new classProductCartDetails(
+      productId,
+      colorValue,
+      quantityValue
+    );
+
+    console.table(objectProduct);
+
+    alert("Votre produit a bien été ajouté au panier!");
+    addedToCart(objectProduct);
+  }
+
+  if (quantityValue <= 0) {
+    alert("Attention! La quantité saisie est invalide");
+  }
+  if (colorValue == 0) {
+    alert("Attention! Vous devez choisir une couleur");
+  }
 });
